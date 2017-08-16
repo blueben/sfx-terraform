6d70424cc175f77c3193b1fcd9047916057b170e
Benjamin Krueger <ben.krueger@nordstrom.com>
2017-08-16 01:25:45 -0700

    A clean start

--- main.tf
+++ main.tf
@@ -1,19 +1,7 @@
 /*
-Terraform config for Seattlefenix
+Description: 
+Maintainer: Benjamin Krueger
+Maintainer_email: benjamin@seattlefenix.net
+
+Copyright 2017 Benjamin Krueger
 */
-
-// Use the VPC module
-
-module "west01" {
-	source 					= "./vpc"
-	alias 					= "west"
-	region 					= "us-west-2"
-	domain 					= "${var.domain}"
-	vpc_count				= "${var.vpc_count}"
-	network 				= "${var.vpc_network}"
-	public_subnets 	= "${var.vpc_public_subnets}"
-	private_subnets = "${var.vpc_private_subnets}"
-	prefix_extent		= "${var.vpc_prefix_extent}"
-	mx_nodes 				= "${var.mx_nodes}"
-	ssh			 				= "${var.ssh}"
-}
